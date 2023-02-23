5247605a316c3a62782c20efe6a7aaf70c69682f
PierreWTH <wietrichpierre@gmail.com>
2023-02-23 16:15:49 +0100

    css fait

--- index.php
+++ index.php
@@ -3,35 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+
     <title>Ajout Produit</title>
 </head>
-<body>
-    <h1>Ajouter un produit</h1>
-     
-    <!-- action : indique la cible du formulaire, méthod : méthode par laquelle les données HTTP seront transmises -->
-    <form action="traitement.php" method="post">
-        <p>
-            <label>
-                Nom du produit :
-                <input type="text" name="name">
-            </label>
-        </p>
-        <p>
-            <label>
-                Prix du produit : 
-                <input type="number" step="any" name="price">
-            </label>
-        </p>
-        <p>
-            <label>
-                Quantité désirée :
-                <input type="number" name="qtt" value="1">
-            </label>
-        </p>
-        <p>
-            <input type = "submit" name = "submit" value="Ajouter le produit">
-        </p>
-    </form>
     
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="recap.php">Mon panier</a></li>
+        </ul>
+    </nav>
+
+    <h1>AJOUTER UN PRODUIT</h1>
+
+    <div class="form">
+    <!-- action : indique la cible du formulaire, méthod : méthode par laquelle les données HTTP seront transmises -->
+        <form action="traitement.php" method="post" class="form-group">
+            <p>
+                <label>
+                    Nom du produit :
+                    <input class="choice form-control form-control"type="text" name="name">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Prix du produit : 
+                    <input class = "choice form-control"type="number" step="any" name="price">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Quantité désirée :
+                    <input class = "choice form-control" type="number" name="qtt">
+                </label>
+            </p>
+            <p>
+                <input class="submit" type = "submit" name = "submit" value="Ajouter le produit">
+            </p>
+        </form>
+    </div> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
--- recap.php
+++ recap.php
@@ -7,9 +7,18 @@ session_start()
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Récapitulatif des produits</title>
 </head>
 <body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="recap.php">Mon panier</a></li>
+        </ul>
+    </nav>
+    <h2>MON PANIER</h2>
     <?php
     // Verification si la clé product n'existe pas ou ne contient aucune donnée
     if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
@@ -19,14 +28,14 @@ session_start()
     else{
 
         echo
-            "<table>",
+            "<table class='table'>",
             "<thead>",
                 "<tr>",
                     "<th>#</th>",
-                    "<th>Name</th>",
-                    "<th>Price</th>",
-                    "<th>Quantity</th>",
-                    "<th>Total</th>",
+                    "<th scope='col'>Name</th>",
+                    "<th scope='col'>Price</th>",
+                    "<th scope='col'>Quantity</th>",
+                    "<th scope='col'>Total</th>",
                 "</tr>",
             "</thead>",
             "<tbody";
@@ -35,7 +44,7 @@ session_start()
         //boucle pour chaque donnée de SESSION : index numerote chaque produit, product contient le produit
 
         $totalGeneral = 0;
-        
+
         foreach($_SESSION['products'] as $index => $product){
             echo "<tr>",
                     "<td>".$index."</td>",
@@ -44,27 +53,19 @@ session_start()
                     "<td>".$product['qtt']."</td>",
                     "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
                 "</tr>";
+                // Ajout de la valeur du produit parcoura au total general
                 $totalGeneral += $product['total'];
         }
         echo "<tr>",
                 "<td colspan=4> Total General : </td>",
                 "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp€</strong><td>",
             "</tr>",
-            "</tbody>";
-            
-
-        
-        echo "</tbody>",
+            "</tbody>",
             "</table";
 
     }
     
-    
-    
-    
-    
-    
-    
     ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
