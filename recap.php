249380c2d9162fedd95b503d13e3543a08e24fa4
PierreWTH <wietrichpierre@gmail.com>
2023-02-23 08:57:21 +0100

    first commit

---
+++ recap.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajout Produit</title>
+</head>
+<body>
+    <h1>Ajouter un produit</h1>
+    <form action="traitement.php" method="post">
+        <p>
+            <label>
+                Nom du produit :
+                <input type="text" name="name">
+            </label>
+        </p>
+        <p>
+            <label>
+                Prix du produit :
+                <input type="number" name="price">
+            </label>
+        </p>
+        <p>
+            <label>
+                Quantité désirée :
+                <input type="number" name="qtt">
+            </label>
+        </p>
+    </form>
+    
+</body>
+</html>
